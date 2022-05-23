3461f20f96ef7a686c4c8babfcdc799a0de45c83
ZhekaProff <Zhekakad@ya.ru>
2022-05-23 18:46:25 +0900

    // Показать числа Фибоначчи меньше заданного числа N

---
+++ Lesson03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать числа Фибоначчи меньше заданного числа N
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()?? "");
+
+int firstValue  = 0;
+int secondValue = 1;
+int result      = 0;
+Console.Write($" ghj {firstValue} {secondValue} ");
+
+while(firstValue + secondValue < N)
+{
+    result = firstValue + secondValue;
+    Console.Write(result + " ");
+    firstValue = secondValue;
+    secondValue = result;
+}
